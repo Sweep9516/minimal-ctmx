f1c908fbd2eaa0616a2ceacce2e976e672fa2fbc
dtou <dieter.tournoy@gmail.com>
2021-10-24 18:37:48 +0200

    add project to git

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to minimal-ctmx
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dtou/minimal_ctmx_test.clj
@@ -0,0 +1,7 @@
+(ns dtou.minimal-ctmx-test
+  (:require [clojure.test :refer :all]
+            [dtou.minimal-ctmx :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
